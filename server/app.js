083f0093a97fe1a9d2c36786eb17eafd244cd13d
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2026-03-17 13:10:32 +0300

    html emails

--- server/app.js
+++ server/app.js
@@ -13,10 +13,7 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/saas', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/saas')
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
